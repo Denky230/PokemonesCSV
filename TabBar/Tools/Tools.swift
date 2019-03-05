ec9a1aed28cb927b2bb8f8e313b3a0d9b34eaaed
Denky <sorek230@gmail.com>
2019-03-05 20:26:10 +0100

    Get UIImage from URL improved

--- TabBar/Tools/Tools.swift
+++ TabBar/Tools/Tools.swift
@@ -20,9 +20,14 @@ class Tools {
             let csv = try CSVReader(contentsOfURL: path)
             // Create new Pokemon for each row in CSV
             for row in csv.rows {
+                // Get UIImage from CSV URL
+//                let url = URL(string: row["PNG"]!)
+//                let data = try Data(contentsOf: url!)
+//                let image: UIImage = UIImage(data: data)!
+                
                 let pokemon = Pokemon(
                     id: Int(row["Number"]!)!,
-                    sprite: UIImage(),
+                    sprite: UIImage(named: "pikachu")!,
                     name: row["Pokemon"]!,
                     type: PokemonType(rawValue: row["Type 1"]!)!,
                     subtype: PokemonType(rawValue: row["Type 2"]!)!,
@@ -30,14 +35,14 @@ class Tools {
                 )
                 
                 // Get UIImage from CSV URL
-                getImageFromURL(imageURL: row["PNG"]!) { (imgRecovered) -> Void in
-                    if let image = imgRecovered {
-                        DispatchQueue.main.async {
-                            pokemon.sprite = image
-                            return
-                        }
-                    }
-                }
+//                getImageFromURL(imageURL: row["PNG"]!) { (imgRecovered) -> Void in
+//                    if let image = imgRecovered {
+//                        DispatchQueue.main.async {
+//                            pokemon.sprite = image
+//                            return
+//                        }
+//                    }
+//                }
                 
                 pokemones.append(pokemon)
             }
